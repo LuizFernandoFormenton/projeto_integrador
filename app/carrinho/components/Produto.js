2c495f2c495267ada244b7a00b22bc57325e909d
Daniel do Prado Werneck <danielpwerneck@gmail.com>
2025-01-29 22:16:55 -0300

    Pagina de carrinho componetezada

---
+++ app/carrinho/components/Produto.js
@@ -0,0 +1,18 @@
+function Produto() {
+    return ( 
+
+        
+        <div class="centralizar">
+    <img src="https://placehold.co/120"/> 
+  
+        <h2>CROPPED RAGLAN ITALIC</h2>
+        <p>Tamanho: <strong>G</strong></p>
+        <p>Total <strong>R$ 99,99</strong></p>
+        </div>
+
+
+
+     );
+}
+
+export default Produto;
--- app/carrinho/page.js
+++ app/carrinho/page.js
@@ -1,34 +1,20 @@
+import Produto from "./components/Produto";
+import Quantidade from "./components/Quantidade";
+
 function carrinho(){
     return(
 
         <div>
-             <h1>Carrinho:</h1>
-
+            <h1>Carrinho:</h1>
 <hr/>
-<div class="centralizar">
-    <img src="https://placehold.co/120"/> 
-  <div>
-        <h2>CROPPED RAGLAN ITALIC</h2>
-        <p>Tamanho: <strong>G</strong></p>
-        <p>Total <strong>R$ 99,99</strong></p>
-    </div>
-</div>
+    <Produto/>
+    <Quantidade/>
 
-<br/>
-<p><small>Quantidade</small></p>
-<button className="buttonnegativo"> - </button>
-<input className="imputmenor"/> 
-<button className="buttonpositivo">+</button>
-
-
-<hr/>
-
-<hr/>
 
 <p><strong>Total ValorR$99,99</strong></p>    
 
 <button class="buttonfinalizarcompras"> Finalizar Compra </button>            
-
+<hr/>
 
         </div>
     );
