e3bd98046d4de62fb5f4a35062518e7a02d47aa8
Pedro Leite <pedrolte11@hotmail.com>
2025-01-29 22:22:57 -0300

    Cabecalho e Rodape
    Fiz os componentes de cabecalho e rodape

--- app/components/Cabecalho.js
+++ app/components/Cabecalho.js
@@ -1,16 +1,18 @@
 function Cabecalho () {
     return (
-        <div className="flex justify-center bg-red-500 pt-6 pb-20 fixed w-full gap-10 ">
-        <img className="w-90 p-50 fixed -left-40 -top-50  " src="https://placehold.co/200"/>
+    <div>
+        <div className="flex justify-center bg-red-500 w-full gap-20 items-center text-white fixed">
+        <img className="w-20 justify-start" src="https://placehold.co/200"/>
         <p> <a href="#"> Camisetas Femininas </a></p>
         <p> <a  href="#"> Camisetas Masculinas </a></p>
         <p> <a  href="#"> Camisetas Unisex </a></p>
         <p> <a  href="#"> Lançamentos </a></p>
         <p> <a  href="#"> Promoções </a></p>
         <label>
-            <input className="w-200 rounded-lg" type="text" placeholder="🔍 O que você está procurando?"/>           
+            <input className="w-300 rounded-lg p-5" type="text" placeholder="🔍 O que você está procurando?"/>           
         </label>
-    </div>  
+    </div>
+    </div>
 
       );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
 
 export default function Home() {
   return (
@@ -7,55 +8,26 @@ export default function Home() {
 
       <Cabecalho/>
 
-   
-     <div className="barraLateral">
-        <button className="botoes"><i class="fa-regular fa-user"></i> </button>
-        <button className="botoes"><i class="fa-regular fa-heart"></i></button>
-        <button className="botoes"><i class="fa-solid fa-cart-shopping"></i></button>
-    </div>
-    <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      
 
-   <main>
-        <h1 className="textosite">Lançamentos</h1>
-        <br/>
-        <div>            
-            <img className="camisetas" src="../imagens/imagens_tela_inicial/camisetas_fundo/Mockup - Camiseta (1).png" width="500"/>
-            <img className="camisetas" src="../imagens/imagens_tela_inicial/camisetas_fundo/Mockup - Camiseta 2 (1).png" width="500"/>
-        </div>
-    </main>
+      <Rodape/>
 
-    <footer>
-    <div className="rodape">
-
-        <div>
-            <h1> Produtos</h1>
-            <ul>
-                <li> <a href="#">Camisas</a></li>
-                <li><a href="#">Acessórios</a></li>
-            </ul>
-        </div>
-
-        <div>
-            <h1>Atendimento e Suporte</h1>
-            <ul>
-                <li><a href="#">Central de atendimento</a></li>
-                <li><a href="#">Dúvidas sobre produto</a></li>
-                <li><a href="#">FAQ</a></li>
-            </ul>
-        </div>
-    
-        <div>
-        <ul>
-            <h1> Institucional</h1>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Insvestidores</a></li>
-        </ul>
-
-        </div>
-    </div> 
-
-
-</footer>
     </div>
   );
 }
