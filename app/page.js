7109fc51ea0bf8a7572bcd908035424f32258f40
Pedro Leite <pedrolte11@hotmail.com>
2025-01-29 21:13:53 -0300

    Pagina inicial
    Transferi o HTML da pagina inicial. Terminando de deixar a formatação no estilo tailwind

--- app/page.js
+++ app/page.js
@@ -1,101 +1,61 @@
 import Image from "next/image";
+import Cabecalho from "./components/Cabecalho";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <Cabecalho/>
+
+   
+     <div className="barraLateral">
+        <button className="botoes"><i class="fa-regular fa-user"></i> </button>
+        <button className="botoes"><i class="fa-regular fa-heart"></i></button>
+        <button className="botoes"><i class="fa-solid fa-cart-shopping"></i></button>
+    </div>
+    <br/>
+
+   <main>
+        <h1 className="textosite">Lançamentos</h1>
+        <br/>
+        <div>            
+            <img className="camisetas" src="../imagens/imagens_tela_inicial/camisetas_fundo/Mockup - Camiseta (1).png" width="500"/>
+            <img className="camisetas" src="../imagens/imagens_tela_inicial/camisetas_fundo/Mockup - Camiseta 2 (1).png" width="500"/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    </main>
+
+    <footer>
+    <div className="rodape">
+
+        <div>
+            <h1> Produtos</h1>
+            <ul>
+                <li> <a href="#">Camisas</a></li>
+                <li><a href="#">Acessórios</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h1>Atendimento e Suporte</h1>
+            <ul>
+                <li><a href="#">Central de atendimento</a></li>
+                <li><a href="#">Dúvidas sobre produto</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+    
+        <div>
+        <ul>
+            <h1> Institucional</h1>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Insvestidores</a></li>
+        </ul>
+
+        </div>
+    </div> 
+
+
+</footer>
     </div>
   );
 }
