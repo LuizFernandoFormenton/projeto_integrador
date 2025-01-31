4e110918932b79876c585a579dbd24c56aa0c878
ArthurSilva175 <arthurskiinwalker16@gmail.com>
2025-01-30 22:01:10 -0300

    aula 30_01_25

--- app/perfil_de_usuario/page.js
+++ app/perfil_de_usuario/page.js
@@ -1,35 +1,36 @@
-
-import Perfil from "./components/Perfil";
-import Login from "../login/page";
-
 function components () {
     return ( 
-    <div>
-        <Perfil/>
-        <Login/>
-       
+    <div className=" justify-center flex my-24">
+        <div className="justify-items-center w-48 rounded-xl shadow-md p-8 ring-red-1000 ">
+
+    
+            <h1 className="text-red-600 text-3xl">Meu Perfil</h1>
+
+      
+        <div className="flex gap-5 flex text-center">
+            <img width="100" height="100" src="https://placehold.co/1000" className="size-24 rounded-full bg-conic from-blue-600 to-sky-400 to-50% items-center"></img>
+
+        </div>    
+                
+        <div className="text-center">
+
+             <p><strong>Nome</strong></p>
+            <input type="text" placeholder="Digite seu nome"></input>
+        </div>
+        
 
 
-        <p><strong>Nome*</strong></p>
-        <input type="text" placeholder="Digite seu nome"></input>
-        <input readOnly type="datetime-local" placeholder=""></input>
-        <br/>
-
-        <p><strong>Telefone*</strong></p>
+        <p><strong>Telefone</strong></p>
         <input type="number" placeholder="Digite seu telefone"></input>
-        <input type="number" placeholder="Digite seu celular"></input>
+        <br/>
+        <br/>
+        <input type="number" placeholder="Digite seu CPF"></input>
         <br/>
         <br/>
         <button>Salvar</button>
         <br/>
-        <hr/>
 
-        <h1>Historico de Compras*</h1>
-
-        <p><strong>Boné Dad Hat Court Sports</strong></p>
-        <p>Cor: <strong>Azul</strong></p>
-        <p>Tamanho: <strong>U</strong></p>
-        <img width="50" height="50" src="https://placehold.co/500"></img>
+        </div>
 
     </div>
      );
