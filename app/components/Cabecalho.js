57a2e6225c3597379a0e3c0608a810ad330340af
Pedro Leite <pedrolte11@hotmail.com>
2025-01-30 22:36:19 -0300

    Estilizei e fiz o cabeçalho e rodape
    Fiz o cabeçalho e rodape no layout

--- app/components/Cabecalho.js
+++ app/components/Cabecalho.js
@@ -1,18 +1,18 @@
 function Cabecalho () {
     return (
-    <div>
-        <div className="flex justify-center bg-red-500 w-full gap-20 items-center text-white fixed">
-        <img className="w-20 justify-start" src="https://placehold.co/200"/>
-        <p> <a href="#"> Camisetas Femininas </a></p>
-        <p> <a  href="#"> Camisetas Masculinas </a></p>
-        <p> <a  href="#"> Camisetas Unisex </a></p>
-        <p> <a  href="#"> Lançamentos </a></p>
-        <p> <a  href="#"> Promoções </a></p>
+    <main>
+        <div style={{ background: "linear-gradient(to right, white, #e5e5e5)"}} className="flex  h-20 w-full items-center fixed gap-20 px-8 ">
+        <img className="w-20" src="./imagens/logo.png"/>
+        <p>  Camisetas Femininas</p>
+        <p> Camisetas Masculinas </p>
+        <p> Camisetas Unisex </p>
+        <p> Lançamentos </p>
+        <p> Promoções </p>
         <label>
-            <input className="w-300 rounded-lg p-5" type="text" placeholder="🔍 O que você está procurando?"/>           
+            <input className="rounded-xl p-2" type="text" placeholder="🔍 O que você está procurando?"/>           
         </label>
     </div>
-    </div>
+    </main>
 
       );
 }
--- app/components/Rodape.js
+++ app/components/Rodape.js
@@ -1,35 +1,33 @@
 function Rodape() {
     return ( 
-<div>
-    <div className="bg-black text-white p-20 flex-col">
+<footer className="mt-40">
+    <div className="bg-black text-white p-2 flex justify-center">
 
-        <div className="text-center">
-            <h1> Produtos</h1>
-            <ul>
-                <li> <a href="#">Camisas</a></li>
-                <li><a href="#">Acessórios</a></li>
-            </ul>
+        <div className="text-center px-12 ">
+            <p> Produtos</p>
+            <br/>
+                <p className="pb-2">Camisas</p>
+                <p className="pb-2">Acessórios</p>
         </div>
 
-        <div>
-            <h1>Atendimento e Suporte</h1>
-            <ul>
-                <li><a href="#">Central de atendimento</a></li>
-                <li><a href="#">Dúvidas sobre produto</a></li>
-                <li><a href="#">FAQ</a></li>
-            </ul>
+        <div className="text-center px-12">
+            <p>Atendimento e Suporte</p>
+            <br/>
+                <p className="pb-2">Central de atendimento</p>
+                <p className="pb-2">Dúvidas sobre produto</p>
+                <p className="pb-2">FAQ</p>
+
         </div>
     
-        <div>
-        <ul>
-            <h1> Institucional</h1>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Insvestidores</a></li>
-        </ul>
-
+        <div className="text-center px-12">
+        <p> Institucional</p>
+        <br/>
+            <p className="pb-2">Sobre</p>
+            <p className="pb-2">Investidores</p>
         </div>
+
     </div> 
-</div>
+</footer>
      );
 }
 
