9bd9b20d8ed78629b1efaf32a96c07b2995d259e
Luiz Fernando <luizformenton14@gmail.com>
2025-01-30 22:34:20 -0300

    AULA PROJETO, ESTILZIANDO AS TELAS
    DIA 30/01/2025

--- app/login/components/Botoes.js
+++ app/login/components/Botoes.js
@@ -1,8 +1,8 @@
-function Botoes() {
+function Botoes(attr) {
     return ( 
         <div>  
             <form> 
-               <button> Entrar </button>
+               <button className={"  text-white p-2 border-none rounded-lg bg-red-500 shadow-lg shadow-red-500/50 w-full "+attr.className} > {attr.botoes} </button>
            </form>
         </div>
      );
