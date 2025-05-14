a58022d9c894c7dd9331207745ba1b09b9a53ecc
Pedro Leite <pedrolte11@hotmail.com>
2025-05-14 20:37:34 -0300

    Mudanças no código

--- app/page.js
+++ app/page.js
@@ -2,64 +2,26 @@
 import Image from "next/image";
 import Cabecalho from "./components/Cabecalho";
 import Rodape from "./components/Rodape";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Produtos from "./components/Produtos";
 
 export default function Home() {
   const [ precoProduto, alteraPreco] = useState(25);
 
-  const [produtos, alteraProdutos] = useState([
-    {
-      id: 0,
-      desconto: 5,
-      nome: "Camisa Básica",
-      preco: 23.99,
-      tamanho: "PP, P, M, G, GG",
-      cor: ["Preto", "Rosa", "Verde"],
-      img: "camisetas_png/Mockup - Camiseta 2 (1).png",
-      avaliacao: "⭐⭐⭐"
-    },
-    {
-      id: 1,
-      desconto: 10,
-      nome: "Camisa Street",
-      preco: 35.90,
-      tamanho: "P, M, G",
-      cor: ["Branco", "Azul", "Cinza"],
-      img: "camisetas_png/Mockup - Camiseta 3 (1).png",
-      avaliacao: "⭐⭐⭐⭐⭐"
-    },
-    {
-      id: 2,
-      desconto: 15,
-      nome: "Camisa Oversized",
-      preco: 49.99,
-      tamanho: "M, G, GG",
-      cor: ["Preto", "Branco"],
-      img: "camisetas_png/Mockup - Camiseta 4 (1).png",
-      avaliacao: "⭐⭐⭐⭐⭐"
-    },
-    {
-      id: 3,
-      desconto: 5,
-      nome: "Camisa Retrô",
-      preco: 29.90,
-      tamanho: "PP, P, M, G",
-      cor: ["Vermelho", "Amarelo", "Azul"],
-      img: "camisetas_png/Mockup - Camiseta 5 (1).png",
-      avaliacao: "⭐⭐⭐⭐"
-    },
-    {
-      id: 4,
-      desconto: 20,
-      nome: "Camisa Esportiva",
-      preco: 55.10,
-      tamanho: "P, M, G, GG",
-      cor: ["Preto", "Azul"],
-      img: "camisetas_png/Mockup - Camiseta 6.png",
-      avaliacao: "⭐⭐⭐⭐⭐"
-    },
-  ]);
+  const [produtos, alteraProdutos] = useState([]);
+
+  async function buscarCamisas(){
+    const response = await axios.get("http://localhost:4000/produtos")
+    console.log(response.data)
+  }
+
+
+
+  useEffect (()=>{
+    buscarCamisas()
+  }, [])
+
 
 
   return (
