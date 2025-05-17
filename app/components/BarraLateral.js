1c63fb67ea00b2759076bce67dbf18c1d7175bf2
ArthurSilva175 <arthurskiinwalker16@gmail.com>
2025-05-16 22:02:04 -0300

    tela de perfil

--- app/components/BarraLateral.js
+++ app/components/BarraLateral.js
@@ -4,14 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function BarraLateral() {
 
+    function direcionaPerfil (){
+        const usuario = JSON.parse(localStorage.getItem("usuario"));
 
+        if (!usuario || !usuario.id) {
+            window.location.href = "/login"; 
+        }else {
+            window.location.href = "/perfil_de_usuario";
+        }
+    }
 
     
     return ( 
 
     <div className="fixed right-0 top-20 h-48 w-12 bg-stone-100/60 rounded-bl-lg border-none border-gray-200 justify-items-center z-20">
 
-        <button className="bg-transparent border-none my-8 cursor-pointer" onClick={()=> window.location.href="/perfil_de_usuario"}>
+        <button className="bg-transparent border-none my-8 cursor-pointer" onClick={()=>{direcionaPerfil()}}>
             <FontAwesomeIcon icon={faUser} className="text-3xl m-2" />
         </button>
         <br/>
