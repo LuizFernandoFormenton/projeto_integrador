4f3d80af02304cfabb2c3438c691f6ee6ed8e3dc
Luiz Fernando <luizformenton14@gmail.com>
2025-05-28 22:13:00 -0300

    BigLu ajeitando as cagadas kk

--- app/login/page.js
+++ app/login/page.js
@@ -57,7 +57,7 @@ function Login () {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="rounded-lg p-10 shadow-[0px_0px_30px] shadow-gray-200 w-full max-w-sm">
-                <h1 className="text-center font-sans text-red-500 text-2xl mb-4">Login</h1>
+                <h1 className="text-center font-sans text-red-500 mb-4">Login</h1>
                 <hr className="mb-4" />
 
                 {/* Campo de e-mail */}
@@ -71,38 +71,43 @@ function Login () {
                     />
                 </div>
 
+
+                {/* Campo de senha com ícone de olho embutido  */}
+                
+
+                    <div className="mb-4">
+                    <label className=" block mb-1 text-gray-700">Senha</label>
+                    <div className="relative">
+                    <input
+                        type={mostrarSenha ? "text" : "password"}
+                        onChange={(e) => AlteraSenha(e.target.value)}
+                        className="border border-black p-2 w-full rounded "
+                        placeholder="Senha"
+                        />
+                    <button
+                        type="button"
+                        onClick={() => setMostrarSenha(!mostrarSenha)}
+                        className="absolute right-[-14px] top-1/4 -translate-y-1/2 text-gray-600 bg-transparent border-none p-0 m-0 cursor-pointer"
+                        style={{ outline: "none", boxShadow: "none" }}
+                    >
+                        {mostrarSenha ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </button>
+                    </div>
+                    </div>
+
+
                 {/* Mensagens de erro */}
                 {erroSenha && (
-                    <div className="bg-red-500 text-white px-2 py-1 rounded my-2">
-                        <p>Por favor digite sua senha</p>
+                    <div className=" text-red-500   rounded my-2">
+                        <p>Por favor digite sua senha!</p>
                     </div>
                 )}
 
                 {erroUsuario && (
-                    <div className="bg-red-500 text-white px-2 py-1 rounded my-2">
-                        <p>Usuário não encontrado</p>
+                    <div className=" text-red-500   rounded my-2">
+                        <p>Usuário não encontrado!</p>
                     </div>
                 )}
-
-                {/* Campo de senha com ícone de olho embutido */}
-                <div className="mb-4">
-                    <label className="block mb-1 text-gray-700">Senha</label>
-                    <div className="relative">
-                        <input
-                            type={mostrarSenha ? "text" : "password"}
-                            onChange={(e) => AlteraSenha(e.target.value)}
-                            className="border border-black p-2 w-full rounded pr-10"
-                            placeholder="Senha"
-                        />
-                        <button
-                            type="button"
-                            onClick={() => setMostrarSenha(!mostrarSenha)}
-                            className="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-600"
-                        >
-                            {mostrarSenha ? <EyeOff size={20} /> : <Eye size={20} />}
-                        </button>
-                    </div>
-                </div>
 
                 {/* Links e botões */}
                 <a href="#" className="text-blue-600 text-sm">Esqueceu sua senha?</a>
