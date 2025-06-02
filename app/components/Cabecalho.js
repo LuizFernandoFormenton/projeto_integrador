c71ad3e5491d2cfeeae1ea60d5f1c5a4f72b2e8c
Luiz Fernando <luizformenton14@gmail.com>
2025-06-02 19:18:19 -0300

    ue

--- app/components/Cabecalho.js
+++ app/components/Cabecalho.js
@@ -84,15 +84,6 @@ function Cabecalho({ produtos, onSearch }) {
                     />
 
                     <div className="relative" ref={menuRef}>
-                        <button
-                            className={`flex items-center gap-2 py-2 px-4 font-medium rounded-md shadow-sm transition-all duration-200
-                                    ${menuAberto ? 'bg-black text-white' : 'bg-white text-gray-800 border border-gray-300 hover:bg-black hover:text-white'}`}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
-                            </svg>
-                            FILTRAR
-                        </button>
 
                         {menuAberto && (
                             <div
@@ -133,13 +124,6 @@ function Cabecalho({ produtos, onSearch }) {
                             </div>
                         )}
                     </div>
-
-                    {/* Creative Xpression logo */}
-                    <img 
-                        src="https://i.postimg.cc/SKzHZSPC/Chat-GPT-Image-19-de-mai-de-2025-22-01-57.png" 
-                        alt="Creative Xpression" 
-                        className="h-12 mx-10 hidden lg:block"
-                    />
                 </div>
 
                 <div className="w-full flex justify-end pr-20">
